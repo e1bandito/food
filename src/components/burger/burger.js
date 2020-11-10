81ab88335e000fe90aa7f630b37e1610f24febb9
e1bandito <aleksandr.shitov@mail.ru>
2020-11-10 12:16:57 +0300

    add adaptive

--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -2,13 +2,13 @@ import $ from "jquery";
 
 $('.burger').on('click', function () {
   $(this).toggleClass('burger--open');
-  $('.nav').toggleClass('nav--open');
+  $('.nav__wrap').toggleClass('nav__wrap--open');
   $('body').toggleClass('body-lock');
 });
 
 $('.nav__link').on('click', function () {
-  if ('nav--open') {
-    $('.nav').removeClass('nav--open');
+  if ('nav__wrap--open') {
+    $('.nav__wrap').removeClass('nav__wrap--open');
     $('.burger').removeClass('burger--open');
     $('body').removeClass('body-lock');
   }
