d7ac3876789c90a6d3be17e0eed0ce94db4b6af2
e1bandito <aleksandr.shitov@mail.ru>
2020-11-11 13:37:25 +0300

    fix iphone mobile scroll on main nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 import { components } from '../components/components';
 import {} from './import/main';
+
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
