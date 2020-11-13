45c61da268af26277a1e3ff6d4d74d71a2e97f62
e1bandito <aleksandr.shitov@mail.ru>
2020-11-13 16:28:38 +0300

    Update 2020-11-13T13:28:17.951Z

--- src/blocks/speciality/speciality.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import slick from 'slick-carousel';
-
-$('.speciality__slider').slick({
-  dots: true,
-  arrows: false,
-  // fade: true
-});
--- src/js/import/main.js
+++
@@ -1,4 +0,0 @@
-import {slider} from '../../blocks/speciality/speciality';
-import {tabs} from '../../blocks/menu/menu';
-import {bookForm} from '../../blocks/book-form/book-form';
-import {contactForm} from '../../blocks/contact/contact';
